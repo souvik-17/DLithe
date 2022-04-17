a48b041fbf8b2ef5d5b0ddbe351afcc76efd948a
Souvik Sarkar <65687134+souvik-17@users.noreply.github.com>
2022-04-17 09:23:10 +0530

    Add files via upload
    1. Create a class
    2. Implement main method
    3. Write logical comments

---
+++ Backend/Assignment1.java
@@ -0,0 +1,15 @@
+/**
+ * @author- souvik sarkar
+ * @since 12.04.2022
+ * @version- 1.0
+ */
+
+package practice;     //package name
+
+public class Assignment1 {       //creating class
+    public static void main(String[] args) {     //main method
+
+        System.out.println("hello world");       //printing "hello world"
+    }
+
+}
